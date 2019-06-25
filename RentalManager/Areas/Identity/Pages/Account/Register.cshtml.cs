ce179e67cb438c3b26a77c1eaee70b38fc87a423
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-06-25 19:58:10 -0400

    99% sure identity works

--- RentalManager/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RentalManager/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -82,7 +82,7 @@ namespace RentalManager.Areas.Identity.Pages.Account
                 {
                     FirstName = Input.FirstName,
                     LastName = Input.LastName,
-                    Email = Input.Email
+                    UserName = Input.Email, Email = Input.Email
                 };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
