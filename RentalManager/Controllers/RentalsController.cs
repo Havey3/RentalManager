2d39e53f2de7044ea0bd410daa84ded7e4a80745
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-07-10 13:52:30 -0400

    got stats to work, going to try and add charts

--- RentalManager/Controllers/RentalsController.cs
+++ RentalManager/Controllers/RentalsController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using RentalManager.Data;
 using RentalManager.Models;
 using RentalManager.Models.ViewModel;
+using RentalManager.Models.ViewModel.Report;
 
 namespace RentalManager.Controllers
 {
@@ -235,9 +236,6 @@ namespace RentalManager.Controllers
 
             //return RedirectToAction(nameof(Index));
         }
-  
-
-
 
 
         // POST: Rentals/Delete/5
@@ -256,7 +254,32 @@ namespace RentalManager.Controllers
         {
             return _context.Rentals.Any(e => e.Id == id);
         }
+
+        //Stats GET
+        [HttpGet]
+        public async Task<IActionResult> Stats()
+        {
+            Stats statsVm = new Stats();
+
+            List<UserRentals> userRentals = await _context.UserRentals
+                .Include(ur => ur.User)
+                .Include(ur => ur.Rental).ToListAsync();
+            // .Include(ur => ur.endDate)
+            //.Include(ur => ur.startDate)
+
+            List<UserRentals> rentedProperties = userRentals.Where(ur => ur.Rental.Id == ur.rentalId).ToList();
+
+            statsVm.topRentals = (from ur in rentedProperties
+                                  group ur by ur.rentalId into gr
+                                  orderby gr.Count() descending
+                                  select new PopularRentals()
+                                  {
+                                      rentalStats = gr.ToList()[0].Rental,
+                                      numberOfRents = gr.ToList().Count()
+                                  }).ToList();
+
+            return View(statsVm);
+        }
     }
-
-
 }
+
---
+++ RentalManager/Models/ViewModel/Stats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RentalManager.Models.ViewModel.Report;
+
+namespace RentalManager.Models.ViewModel
+{
+    public class Stats
+    {
+        public List<PopularRentals> topRentals { get; set; } = new List<PopularRentals>();
+        public List<PopularDate> topDates { get; set; } = new List<PopularDate>();
+
+    }
+}
